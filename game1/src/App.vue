139bb1b5dd00b9f9d0c7e63c5a43e8ae0ad32e01
jeongwon oh <0802ojw@gmail.com>
2023-05-09 09:05:16 +0900

    asdasd

---
+++ game1/src/App.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+      <h1>GameScene: {{currentScene()}} </h1>
+
+      <div v-if="currentScene() == 1">
+        <GameScene1/>
+      </div>
+      <div v-if="currentScene() == 2">
+        <GameScene2/>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+import GameScene1 from './components/GameScene1.vue';
+import GameScene2 from './components/GameScene2.vue';
+
+export default {
+  name: 'App',
+  methods: {
+    currentScene() {
+      return this.$store.state.GameScene_cnt
+    }
+  },
+  data() {
+    return{
+
+    }
+  },
+  components: {
+    GameScene1,
+    GameScene2,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.chat-box{
+  border: 2px solid black;
+  width: 1580px;
+  height: 320px;
+  margin: 5px;
+
+  position:absolute; 
+  bottom: 10px;
+
+  background-color: rgb(159, 212, 233 ,0.5);
+}
+
+.profile-img{
+  border: 2px solid black;
+  width: 280px;
+  height: 280px;
+  margin: 15px 5px 5px 5px ;
+
+  background-color: white;
+}
+
+.text-box{
+  border: 1px solid black;
+  width: 1380px;
+  height: 300px;
+  margin: 10px;
+
+  background-color: rgba(255, 255, 255, 0.1);
+  
+}
+
+.game-background{
+  border: 2px solid black;
+  width: 1600px;
+  height:900px;
+  margin: 10px;
+}
+
+.option{
+  border-radius: 10px;
+  border: 2px solid black;
+  width: 300px;
+  height: 200px;
+  background-color: rgb(159, 212, 233 ,0.5);
+  
+  z-index: 1;
+
+  position: absolute;
+  top: 30%;
+  left: 35%;
+}
+
+.next-button{
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  height: 100px;
+  margin: 5px;
+
+  width: 150px;
+  background-color: rgb(196, 212, 245);
+}
+
+.select-button{
+  position: relative;
+  top: 20px;
+  left: 30px;
+  margin: 20px;
+
+
+  height: 50px;
+  width: 200px;
+  background-color: rgb(196, 212, 245);
+}
+
+.center-character{
+  position: absolute;
+  top: 10%;
+  left: 25%;
+
+  height: 500px;
+  width: 500px;
+   
+}
+
+.background {
+  width: 1596px;
+  height:896px;
+
+  background-size: cover;
+  object-fit: cover;
+  z-index: -1;
+}
+</style>
---
+++ game1/src/components/GameScene2.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="game-background">
+  
+      <div class="center-character">
+        <img v-if="script_cnt>=6" src="@/assets/violet0.png" alt="" class="center-character">  
+      </div>   
+
+      <img v-if="script_cnt>=12" src="@/assets/background0.png" alt="" class="background">
+
+      <div v-if="show_option()" class= "option">
+        <button class="select-button">
+          선택지1 : 게임씬 4로 간다.
+        </button>
+        <br>
+        <button class="select-button">
+          선택지2 : 게임씬 5으로 간다.
+        </button>
+      </div>
+
+      <div class="chat-box d-flex" >
+        <img :src="profile_src" alt ="" class="profile-img">
+        
+        <div class="text-box">
+          <h3>{{script[script_cnt][1]}}</h3>
+          <button @click ="add_script_cnt" class="next-button">다음</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import player from "@/assets/player.png"
+import violet from "@/assets/violet0.png"
+
+export default {
+  data() {
+    
+    return {
+      script_cnt: 0,
+      profile_src: player, // 프로필 이미지 초기화
+      script: [
+        [player,"여기로 왔다는건 무사히 게임씬 2로 왔다는거네요."]  ,  //0
+        [player,"vuex를 사용해서 적절히 state를 변경하는데에 성공하셨겠군요."],
+        [player,"게임씬 1을 안보이게 잘 처리 하셨나요?"],
+        [player,"하지막 아직 구현할게 너무 많아요."],
+        [player,"이런 비주얼 노블 게임은 다른 캐릭터가 화면 중앙에 등장해야해요"],
+        [player,"자 이 대사가 끝나면 등장하게 될거에요"],
+        [player,"짜잔~" ] ,//6
+        [player,"그리고 이 게임개발자는 저녁밥을 먹으면서 이걸 떠올리고 말았죠"],
+        [player,"캐릭터가 등장한다고 해서 끝이 아니에요"],
+        [violet,"다른 캐릭터가 말할땐 그 캐릭터의 이미지가 프로필에 떠야죠"],
+        [player,"대사정보를 담은 리스트에 이미지경로 정보도 담을 필요가 있겠어요"],
+        [violet,"그리고 배경도 띄워야해요."], //11
+        [violet,"짜잔"] ,//12
+        [player,"그리고 곧 선택지가 떠서 또 다른 게임씬으로 가겠죠"],
+        [violet,"이정도면 기본 구색은 갖춘거 같아요"],
+      ]
+    }
+  },
+  computed: {
+  },
+  methods: {
+    add_script_cnt() {
+      this.script_cnt += 1;
+      this.profile_src = this.script[this.script_cnt][0]
+    },
+    show_option () {
+      if(this.script_cnt >= this.script.length-1){
+        return true
+      }
+    },  
+  }
+}
+</script>
+
+<style>
+
+</style>
