d9e6a18c470ba744c382041c552ad6528062f136
ojwojwojw <122504184+ojwojwojw@users.noreply.github.com>
2023-10-13 00:42:59 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,26 +1,40 @@
 N,M = map(int, input().split())
+board = [input() for _ in range(N)]
+ans = N*M
 
-board = [list(input()) for _ in range(N)]
-ans = 64
-
-def fill(y,x,bw):
+def make_acc(bw):
     global ans
-    cnt = 0
-    for i in range(8):
-        for j in range(8):
+    acc = [[0] * M for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            if i > 0 :
+                acc[i][j] += acc[i-1][j]
 
-            if (i+j) %2 :
-                if board[y+i][x+j] == bw:
-                    cnt += 1
-            else:
-                if board[y+i][x+j] != bw:
-                    cnt += 1
-    ans = min (ans , cnt)
+            if j > 0 :
+                acc[i][j] += acc[i][j-1]
 
+            if i>0 and j>0:
+                acc[i][j] -= acc[i-1][j-1]
 
-for y in range(N-7):
-    for x in range(M-7):
-        fill(y,x,'B')
-        fill(y,x,'W')
+            if (i+j)%2 and board[i][j] == bw:
+                acc[i][j] += 1
 
+            if (i+j)%2 == 0 and board[i][j] != bw:
+                acc[i][j] += 1
+
+    for i in range(N-7):
+        for j in range(M-7):
+            cnt = acc[i+7][j+7]
+
+            if i > 0:
+                cnt -= acc[i-1][j+7]
+            if j > 0:
+                cnt -= acc[i+7][j-1]
+            if i > 0 and j > 0:
+                cnt += acc[i-1][j-1]
+
+            ans = min(ans,cnt)
+
+make_acc('B')
+make_acc('W')
 print(ans)
