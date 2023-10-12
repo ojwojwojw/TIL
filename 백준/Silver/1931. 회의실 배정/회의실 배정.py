7a20bdafc00c10c987fdcd2cf880ec935a0d0c0f
ojwojwojw <122504184+ojwojwojw@users.noreply.github.com>
2023-10-12 09:48:09 +0900

    [Silver I] Title: 회의실 배정, Time: 224 ms, Memory: 44580 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,19 @@
+import sys
+
+input=sys.stdin.readline
+meetings= []
+for _ in range(int(input())):
+    start,end = map(int, input().split())
+    meetings.append((end,start))
+
+
+meetings.sort()
+# print(meetings)
+t = 0
+ans = 0
+for end, start in meetings:
+    if t <= start:
+        ans += 1
+        t = end
+
+print (ans)
