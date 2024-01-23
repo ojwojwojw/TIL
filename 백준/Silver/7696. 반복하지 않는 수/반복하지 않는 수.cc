8a129ed0bd3f0e9975f22c69f8ce95ab537ec7b3
ojwojwojw <122504184+ojwojwojw@users.noreply.github.com>
2024-01-23 20:01:03 +0900

    [Silver III] Title: 반복하지 않는 수, Time: 48 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.cc
+++ 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.cc
@@ -10,12 +10,21 @@ int arr[10]; //수열을 기록할
 int answer [1000001][10]; //재귀로 구한 데이터를 넣을 곳
 bool used[10]; //해당 숫자를 사용했는지 기록(visited와 유사)
 
+bool zeroUsed() {
+    for (int i = 0; i < 10; i++) {
+        if (i && used[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 void initialize() {
     cnt = 0;
     fill(begin(arr), end(arr), 0);
     fill(begin(used), end(used), false);
 }
-
 
 void dfs(int m ) {
     if (cnt == 1000001) {
@@ -23,27 +32,18 @@ void dfs(int m ) {
     }
 
     if (m == MAX) {
-
-        //cout << cnt << ' ';
         for (int i = 0; i < MAX; i++) {
             answer[cnt][i] = arr[i];
-            //cout << answer[cnt][i];
         }
-        //cout << '\n';
         cnt++;
         return;
     }
+
     for (int i = 0; i < 10; i++) {
         //used를 검사해서  visited에서 0 이외에 다른것을 썼는지 검사한다
-        bool isNonZeroUsed = false;
-
-        for (int i = 0;i<10; i++) {
-            if (i && used[i]) {
-                isNonZeroUsed = true;
-            }
-        }
+        bool isNonZeroUsed = zeroUsed();
         
-        // 001 002 는 되는데 400은 안되게 조건을 걸자,
+        // 001 002 는 되는데 400은 안되게 used를 변형시킨다.
         if (!isNonZeroUsed) {
             used[0] = false;
         }
@@ -51,13 +51,9 @@ void dfs(int m ) {
         if (!used[i]) { 
             used[i] = true;
 
-            //used를 검사해서  visited에서 0 이외에 다른것을 썼는지 검사한다
-            bool isNonZeroUsed = false;
-            for (int i = 0; i < 10; i++) {
-                if (i && used[i]) {
-                    isNonZeroUsed = true;
-                }
-            }
+            //used를 또 검사해서  used에서 0 이외에 다른것을 썼는지 검사한다
+            isNonZeroUsed = zeroUsed();
+            
 
             //0이외의 것이 쓰이기전의 0과 이후의 0을 구별하자.
             if (!isNonZeroUsed) {
@@ -78,7 +74,6 @@ void dfs(int m ) {
 int main() {
     int input;
     dfs(0);
-    // 각 테스트 케이스에 대해 반복
     while (true) {
         // 숫자 입력
         cin >> input;
