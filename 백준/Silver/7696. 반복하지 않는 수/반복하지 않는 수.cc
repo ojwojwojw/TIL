32d6441ceaed18d066d33c963f0b65bf1e2342b1
ojwojwojw <122504184+ojwojwojw@users.noreply.github.com>
2024-01-23 19:53:47 +0900

    [Silver III] Title: 반복하지 않는 수, Time: 76 ms, Memory: 41084 KB -BaekjoonHub

---
+++ 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.cc
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+# define MAX 8
+
+int cnt = 0; // 몇번째 수열(수) 인지 기록할 변수
+int arr[10]; //수열을 기록할
+int answer [1000001][10]; //재귀로 구한 데이터를 넣을 곳
+bool used[10]; //해당 숫자를 사용했는지 기록(visited와 유사)
+
+void initialize() {
+    cnt = 0;
+    fill(begin(arr), end(arr), 0);
+    fill(begin(used), end(used), false);
+}
+
+
+void dfs(int m ) {
+    if (cnt == 1000001) {
+        return;
+    }
+
+    if (m == MAX) {
+
+        //cout << cnt << ' ';
+        for (int i = 0; i < MAX; i++) {
+            answer[cnt][i] = arr[i];
+            //cout << answer[cnt][i];
+        }
+        //cout << '\n';
+        cnt++;
+        return;
+    }
+    for (int i = 0; i < 10; i++) {
+        //used를 검사해서  visited에서 0 이외에 다른것을 썼는지 검사한다
+        bool isNonZeroUsed = false;
+
+        for (int i = 0;i<10; i++) {
+            if (i && used[i]) {
+                isNonZeroUsed = true;
+            }
+        }
+        
+        // 001 002 는 되는데 400은 안되게 조건을 걸자,
+        if (!isNonZeroUsed) {
+            used[0] = false;
+        }
+
+        if (!used[i]) { 
+            used[i] = true;
+
+            //used를 검사해서  visited에서 0 이외에 다른것을 썼는지 검사한다
+            bool isNonZeroUsed = false;
+            for (int i = 0; i < 10; i++) {
+                if (i && used[i]) {
+                    isNonZeroUsed = true;
+                }
+            }
+
+            //0이외의 것이 쓰이기전의 0과 이후의 0을 구별하자.
+            if (!isNonZeroUsed) {
+                arr[m] = -1;
+            }
+            else {
+                arr[m] = i;
+            }
+
+            dfs(m + 1);
+            used[i] = false;
+        }
+    }
+}
+
+
+// 메인 함수
+int main() {
+    int input;
+    dfs(0);
+    // 각 테스트 케이스에 대해 반복
+    while (true) {
+        // 숫자 입력
+        cin >> input;
+
+        // 입력된 숫자가 0이면 반복 종료
+        if (input == 0) {
+            break;
+        }
+        else {
+            // 변수 및 배열 초기화 함수
+            initialize();
+            // 조건을 만족하는 수를 찾아서 출력
+            for (int i = 0; i < MAX; i++) {
+                if (answer[input][i] != -1) {
+                    cout << answer[input][i];
+                }
+            }
+            cout << '\n';
+        }
+
+    }
+}
