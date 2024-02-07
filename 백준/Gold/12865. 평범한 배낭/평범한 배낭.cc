3af93c250cb9b1a5341ebaac9baaf06f88c38ff0
ojwojwojw <122504184+ojwojwojw@users.noreply.github.com>
2024-02-07 15:53:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,43 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
+int N, K; //물건의 수와 준서가 버틸 수 있는 무게
 int W[101];
 int V[101];
-int DP[101][100001];
-int ans = 0;
-int N, K;
+int DP[100001]; // 1차원 배열도 가능하다.
 
 int main() {
 	cin >> N >> K;
-	
-	for (int i = 1; i <= N; i++) {
+	for (int i = 1; i < N + 1; i++) {
 		cin >> W[i] >> V[i];
 	}
 
-
 	for (int i = 1; i < N + 1; i++) {
-		for (int k = 1; k < K + 1; k++) {
-			//담을 수 있을때와 없을때
-			if (W[i] <= k) {
-				DP[i][k] = max(DP[i-1][k - W[i]] + V[i], DP[i-1][k]);
-			}
-			else {
-				DP[i][k] = DP[i-1][k];
+		for (int j = K; j >= 0; j--) {
+			if (W[i] <= j) {
+				DP[j] = max(DP[j], DP[j - W[i]] + V[i]);
 			}
 		}
 	}
 
-	//for (int row = 1; row < N + 1; row++) {
-	//	for (int limit = 1; limit < K + 1; limit++) {
-	//		cout << DP[row][limit] << ' ';
-	//	}
-	//	cout << endl;
-	//}
+	cout << DP[K];
 
-	cout << DP[N][K];
-
-	return 0;
 }
