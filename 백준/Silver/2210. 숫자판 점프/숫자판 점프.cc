fc475e708407bc3dd981835464ce49ede1583521
ojwojwojw <122504184+ojwojwojw@users.noreply.github.com>
2024-02-13 16:50:28 +0900

    [Silver II] Title: 숫자판 점프, Time: 92 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.cc
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.cc
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
-vector <string> answer;
-char Arr[5][5];
+vector <int> answer;
+int Arr[5][5];
 
-void dfs(int i, int j ,string num , int depth) {
+void dfs(int i, int j ,int num , int depth) {
 	
 	if (depth == 6) {
 		bool duplicated = false;
@@ -25,7 +24,7 @@ void dfs(int i, int j ,string num , int depth) {
 		return;
 	}
 
-	num += Arr[i][j];
+	num = 10*num + Arr[i][j];
 
 	if (0 <= i + 1 && i + 1 < 5 && 0 <= j && j < 5) dfs(i+1, j, num, depth + 1);
 	if (0 <= i  && i < 5 && 0 <= j + 1 && j + 1< 5) dfs(i , j+1, num, depth + 1);
@@ -36,7 +35,6 @@ void dfs(int i, int j ,string num , int depth) {
 
 int main() {
 
-
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
 			cin >> Arr[i][j];
@@ -45,15 +43,10 @@ int main() {
 
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
-			dfs(i, j , "", 0);
+			dfs(i, j , 0, 0);
 		}
 	}
 
-
-	for (auto x : answer) {
-		//cout << x << " ";
-	}
-	//cout << endl;
 	cout << answer.size();
 
 }
