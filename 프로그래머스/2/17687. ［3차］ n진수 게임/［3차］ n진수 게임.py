e613312a007a258812ac95ae8a1a784cbbda4681
ojwojwojw <122504184+ojwojwojw@users.noreply.github.com>
2024-02-26 14:19:30 +0900

    [level 2] Title: [3차] n진수 게임, Time: 14.76 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -2,12 +2,11 @@ mapp = {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',
         6:'6',7:'7',8:'8',9:'9',10:'A',
        11:'B',12:'C',13:'D',14:'E',15:'F'}
 
-
 def solution(n, t, m, p):
     answer = ''
     num_string = ''
     
-    for i in range(n*t*m):
+    for i in range(t*m):
         num = i
         if num == 0:
             num_string += '0'
@@ -18,8 +17,6 @@ def solution(n, t, m, p):
             num = num//n
     
         num_string += temp_str
-    
-    #print(num_string)
     
     for i in range(p-1,t*m,m):
         answer += num_string[i]
