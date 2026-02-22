0f011eee10fede7bfa2f078680417eee93113a70
ojwojwojw <122504184+ojwojwojw@users.noreply.github.com>
2026-02-22 20:55:15 +0900

    [level 2] Title: 주차 요금 계산, Time: 3.76 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -11,23 +11,17 @@ def ToMinute(string):
     return 60*int(h) + int(m)
 
 def solution(fees, records):
-    #print(fees) # 기본시간 , 기본 요금 , 단위 시간 , 단위요금
-    #print(records) # 시간 , 차량번호 ,입/출차여부
-    
     answer = []
     feeTable = {}
     lastDir = {}
-    N = len(records)
-    
+
+    #딕셔너리 자료구조를 <int , list> 로 먼저 형성
     for record in records:
         time, car_num, direction = record.split()
         feeTable[car_num] = []
-        
-    #print(feeTable)
-    
+
     for record in records:
         time, car_num, direction = record.split()
-        #print(time,car_num,direction)
         if direction == "IN":
             feeTable[car_num].append(ToMinute(time))
             lastDir[car_num] = direction
@@ -38,23 +32,17 @@ def solution(fees, records):
     
     #11:59 처리
     for car_num in feeTable.keys():
-        #print(car_num)
         if lastDir[car_num] == "IN" :
             in_time = feeTable[car_num].pop()
             feeTable[car_num].append(ToMinute("23:59") - in_time)
             lastDir[car_num] = direction
             
-
-    print(feeTable)
     # feeTable을 먼저 차량번호가 작은순으로 정렬하고 정답을 계산
     sorted_car_num = sorted(feeTable.keys())
     
-    
     print(sorted_car_num)
     for car_num in sorted_car_num:
-        #print(car_num)
         total_time = sum(feeTable[car_num])
-        #print(total_time)
         if total_time <= fees[0]:
             answer.append(fees[1])
         else:
